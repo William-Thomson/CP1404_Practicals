0ac02b18fff1580582db2bb7c4470acfd0c7aa57
jc428692 <william.thomson@my.jcu.edu.au>
2019-03-21 14:20:38 +1000

    Add starter file.

--- Prac_02/files.py
+++ Prac_02/files.py
@@ -1,30 +1,30 @@
 # Question 1
-# OUTPUT_FILE = 'name.txt'
-#
-# out_file = open(OUTPUT_FILE, 'w')
-#
-# name = str(input("Enter name: "))
-# print(name, file=out_file)
-# out_file.close()
+OUTPUT_FILE = 'name.txt'
+
+out_file = open(OUTPUT_FILE, 'w')
+
+name = str(input("Enter name: "))
+print(name, file=out_file)
+out_file.close()
 
 # Question 2
-# in_file = open('name.txt', 'r')
-# name = in_file.readline().strip()
-# print("Your name is ", name)
-# in_file.close()
+in_file = open('name.txt', 'r')
+name = in_file.readline().strip()
+print("Your name is ", name)
+in_file.close()
 
 # Question 3
-# in_file = open('numbers.txt', 'r')
-# number1 = int(in_file.readline())
-# number2 = int(in_file.readline())
-# print(number1 + number2)
-# in_file.close()
+in_file = open('numbers.txt', 'r')
+number1 = int(in_file.readline())
+number2 = int(in_file.readline())
+print(number1 + number2)
+in_file.close()
 
 # Extension Question
-# in_file = open("numbers.txt", "r")
-# total = 0
-# for line in in_file:
-#     number = int(line)
-#     total += number
-# print(total)
-# in_file.close()
+in_file = open("numbers.txt", "r")
+total = 0
+for line in in_file:
+    number = int(line)
+    total += number
+print(total)
+in_file.close()
