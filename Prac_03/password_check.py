5bcb51daf203cbcfef199b391d01e8ea3669bb76
jc428692 <william.thomson@my.jcu.edu.au>
2019-03-21 14:33:00 +1000

    optimised code.

--- Prac_03/password_check.py
+++ Prac_03/password_check.py
@@ -3,10 +3,9 @@ def get_password():
     password = input('Enter password: ')
     while len(password) <= 3:
         password = input('Invalid password. Enter Password')
-    return password
 
 
-password = get_password()
+get_password()
 
 
 def print_asterisk():
