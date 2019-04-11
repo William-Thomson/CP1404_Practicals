246c296f7ce16a1accacebd2c558d508ace81a4d
jc428692 <william.thomson@my.jcu.edu.au>
2019-04-11 14:27:12 +1000

    Add Guitar class

---
+++ prac_06/guitar.py
@@ -0,0 +1,22 @@
+CURRENT_YEAR = 2019
+VINTAGE_AGE = 50
+
+class Guitar:
+    """Guitar Class"""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Constructs a guitar object"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """returns a string of object values"""
+        return "{} ({}) : {}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        """determines the age of the guitar"""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        return self.get_age() >= VINTAGE_AGE
