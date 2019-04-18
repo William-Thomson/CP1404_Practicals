5302985b6c6500ee0202501f49fa3bc49e19a4c9
willi <william.thomson@my.jcu.edu.au>
2019-04-18 13:41:19 +1000

    Edit Guitars file

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -2,7 +2,7 @@ from prac_06.guitar import Guitar
 
 
 def run_tests():
-    """Tests for Guitar class."""
+    """Tests Guitar class."""
     name = "Gibson L-5 CES"
     year = 1922
     cost = 16035.40
