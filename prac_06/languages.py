131b44558495a22b267ce3fafd154af9820fae47
willi <william.thomson@my.jcu.edu.au>
2019-04-18 13:58:09 +1000

    Add list comprehension to languages.

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -5,16 +5,11 @@ def main():
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    # print(ruby)
-    # print(python)
-    # print(visual_basic)
 
     languages = [ruby, python, visual_basic]
 
     print("The dynamically typed languages are:")
-    for language in languages:
-        if language.is_dynamic():
-            print(language.name)
+    print("\n".join([language.name for language in languages if language.is_dynamic()]))
 
 
 main()
