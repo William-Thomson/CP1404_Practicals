78b9284abfae2d3bf92309150cc16f264a956d26
willi <william.thomson@my.jcu.edu.au>
2019-05-03 12:28:22 +1000

    Add handle calculate method.

--- prac_07/convert_m_km.py
+++ prac_07/convert_m_km.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 
+MILES_TO_KM = 1.60934
+
 
 class MilesToKilometreApp(App):
     def build(self):
@@ -13,5 +15,11 @@ class MilesToKilometreApp(App):
         """Increases/decreases text input number by one."""
         value = float(self.root.ids.input_miles.text) + change
         self.root.ids.input_miles.text = str(value)
+
+    def handle_calculate(self):
+        """Converts from miles to km and prints"""
+        value = float(self.root.ids.input_miles.text) * MILES_TO_KM
+        self.root.ids.output_label.text = str(value)
+
 
 MilesToKilometreApp().run()
