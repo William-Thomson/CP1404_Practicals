bd9dfdbbbca54400e6231bb07f68c6d0564efa27
willi <william.thomson@my.jcu.edu.au>
2019-05-03 12:21:09 +1000

    Add handle increment method.

--- prac_07/convert_m_km.py
+++ prac_07/convert_m_km.py
@@ -9,5 +9,9 @@ class MilesToKilometreApp(App):
         self.root = Builder.load_file('convert_m_to_km.kv')
         return self.root
 
+    def handle_increment(self, change):
+        """Increases/decreases text input number by one."""
+        value = float(self.root.ids.input_miles.text) + change
+        self.root.ids.input_miles.text = str(value)
 
 MilesToKilometreApp().run()
