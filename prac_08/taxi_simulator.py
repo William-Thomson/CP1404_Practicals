e2d9e1c19a94da9fd85c0f71e755e826e5c7d785
willi <william.thomson@my.jcu.edu.au>
2019-05-10 10:00:43 +1000

    Add menu choice 'Drive' Add menu choice 'Quit' Add better naming convention + refactoring

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -5,21 +5,35 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
+    total_bill = 0
     taxis = [Taxi('Prius', 100), SilverServiceTaxi('Limo', 100, 2), SilverServiceTaxi('Hummer', 200, 4)]
     current_taxi = None
     print("Let's Drive!")
-    choice = input(MENU)
-    while choice != "q":
-        if choice == "c":
-            for i, taxi in enumerate(taxis):
-                print("{} - {}".format(i, taxi))
+    menu_choice = input(MENU)
+    while menu_choice != "q":
+        if menu_choice == "c":
+            display_taxis(taxis)
             taxi_choice = int(input("Choose taxi: "))
             current_taxi = taxis[taxi_choice]
-            print("Bill to date: ${}".format(current_taxi.get_fare()))
-        choice = input(MENU)
-        if choice == "d":
-            distance = int(input("Drive how far? "))
-            current_taxi.drive(distance)
+        elif menu_choice == "d":
+            current_taxi.start_fare()
+            distance_to_drive = float(input("Drive how far? "))
+            current_taxi.drive(distance_to_drive)
+            trip_cost = current_taxi.get_fare()
+            print("Your {} trip cost you ${}".format(current_taxi.name, trip_cost))
+            total_bill += trip_cost
+        else:
+            print("Invalid Option")
+        print("Bill to date: ${:.2f}".format(total_bill))
+        menu_choice = str(input(MENU))
+    print("Total Trip Cost: ${:.2f}".format(total_bill))
+    print("Taxis are now:")
+    display_taxis(taxis)
+
+
+def display_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print("{} - {}".format(i, taxi))
 
 
 main()
