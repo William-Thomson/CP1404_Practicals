38ffabd85840c4cec6017cd38be2f2874f9b2de6
willi <william.thomson@my.jcu.edu.au>
2019-05-23 13:09:18 +1000

    Add sort files into new directories.

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -5,7 +5,15 @@ import shutil
 def main():
     print("Starting directory is: {}".format(os.getcwd()))
     os.chdir('FilesToSort')
-    print("New directory is: {}".format((os.getcwd())))
+    for filename in os.listdir('.'):
+        if os.path.isdir(filename):
+            continue
+        file_extension = filename.split('.')[-1]
+        try:
+            os.mkdir(file_extension)
+        except FileExistsError:
+            pass
+        shutil.move(filename, file_extension)
 
 
 main()
