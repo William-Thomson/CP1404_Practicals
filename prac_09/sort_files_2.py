1515bdb9ba255258fc971919cd117bdb2ca17173
willi <william.thomson@my.jcu.edu.au>
2019-05-23 13:22:29 +1000

    Add sort files 2 sorting

---
+++ prac_09/sort_files_2.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+
+def main():
+    extensions_to_category = {}
+    os.chdir('FilesToSort')
+    for filename in os.listdir('.'):
+        if os.path.isdir(filename):
+            continue
+        extension = filename.split('.')[-1]
+        if extension not in extensions_to_category:
+            category = input("What category would you like to sort {} files into?".format(extension))
+            extensions_to_category[extension] = category
+            try:
+                os.mkdir(category)
+            except FileExistsError:
+                pass
+        shutil.move(filename, category)
+
+
+main()
